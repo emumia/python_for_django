bdc46e7a406ab127cac373daca64f683c049b0fc
MD. Mohaiminul Islam Imran <emukhan568@gmail.com>
2023-04-02 15:08:15 +0600

    Add files via upload

---
+++ 1st_assignment.py
@@ -0,0 +1,112 @@
+#1. Print your name.
+#2. Input your batch number using input() function.
+#3. Create 5 different valid Variables and print.
+#4. Create Pascal Case Variable.
+#5. Assign a single value to Multiple variables with memory location.
+#6. Input float value using input().
+#7. a = ‘Welcome to Django for web and Ai’ convert into upper case.
+#8. b = ‘ Python’ remove whitespace.
+#9. c = 60, d= 22 ‘Conversion int to float’.
+#10. Create a dictionary & print your info.1. Print your name.
+
+# Ans no: 1
+print("1st answer")
+print("MD. MOHAIMINUL ISLAM IMRAN")
+print(" ")
+
+#ANS NO 2:
+print("2nd answer")
+
+batch = int(input("Please input your batch number here: "))
+print(f'your batch is django {batch}')
+print(" ")
+
+
+# ans 3 will be at the end
+print("First answer will be at last part due to,for loop")
+print(" ")
+
+
+
+# ans no 4:
+print("4th answer")
+PascalCase = " Pascalcase Variable example is PascalCase "
+print(PascalCase)
+print(" ")
+
+
+ # ans no 5:
+print("5th answer")
+father=brother=husband=friend = "Imran"
+print(id(father))
+print(id(brother))
+print(id(husband))
+print(id(friend))
+print(" ")
+
+
+# ans no 6:
+print("6th answer")
+gpa= float(input("Please input your GPA here: "))
+print(f'your gpa is {gpa}')
+print(" ")
+
+
+
+# ans no 7:
+print("7th answer")
+a = "Welcome to Django for web and Ai"
+print(a.upper())
+print(" ")
+
+
+
+# ans no 8:
+print("8th answer")
+b = '  Python'
+print(b.strip())
+print(" ")
+
+
+
+# ans no 9:
+print("9th answer")
+c = 60
+d= 22
+print(float(c))
+print(float(d))
+print(" ")
+
+
+
+# ans no 10:
+print("10th answer")
+dict ={"name":"Muhammad Imran","age":29,"color":"black","hair_color":"black","height":"5'7"}
+print(dict["name"])
+
+for i in (dict.values()):
+    print(i)
+print(" ")
+
+
+# ans no 3:
+print("3rd answer")
+course_name = 'django 1'
+_coursename = 'django 2'
+CourseName = 'django 3'
+courseName = 'django 4'
+Course2 = 'django 5'
+
+list1 = [course_name, _coursename, CourseName, courseName, Course2]
+for i in list1:
+    print(i)
+
+
+
+
+
+
+
+
+
+
